b5dd08f0947dea6934665826c1bf866adf80db29
ginnfale <takuma199@gmail.com>
2018-01-10 11:30:08 +0900

    Topとりあえずできました

--- WebContent/home.jsp
+++ WebContent/home.jsp
@@ -1,13 +1,46 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="s" uri="/struts-tags" %>
+
+    <%@ page import="com.internousdev.cherry.dto.ProductDTO, java.util.List" %>
+
+<%
+	List<ProductDTO> proList = (List<ProductDTO>) session.getAttribute("proList");	/* Loginから */
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>home</title>
+<title>Cherry</title>
 </head>
 <body>
-<p>商品一覧作成中・・・</p>
+
+<header>
+
+	<a href="/cherry/Cart">カート</a>
+
+	<a href="/cherry/Login">マイページ</a>
+
+	<a href="/cherry/Login">ログイン</a>
+
+</header>
+
+<h1>Cherry</h1>
+
+<!-- ここから商品一覧 -->
+<ul>
+	<% for(ProductDTO pro : proList){ %>
+		<li>
+			<%= pro.getProduct_name() %>
+			<img src="/cherry/img/<%= pro.getImage_file_name() %>">
+		</li>
+	<% }; %>
+</ul>
+
+
+
+<!-- フッター -->
+<footer></footer>
+
 </body>
 </html>
--- src/com/internousdev/cherry/action/TopAction.java
+++ src/com/internousdev/cherry/action/TopAction.java
@@ -1,6 +1,7 @@
 package com.internousdev.cherry.action;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.internousdev.cherry.dao.Product_InfoDAO;
+import com.internousdev.cherry.dto.ProductDTO;
 
 
 @WebServlet("/TopAction")
@@ -19,7 +24,24 @@ public class TopAction extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Product_InfoDAO dao = new Product_InfoDAO();
+		HttpSession session = request.getSession();
+
+		try {
+			List<ProductDTO> proList = dao.pro_list();
+			session.setAttribute("proList", proList);
+
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+
+
+		//フォワード
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/home.jsp"); dispatcher.forward(request, response);
+
 	}
 
 
