683f0b19d52945b35caa3ddcba3ac6284df64a3b
rikakoo <doraemon373@ezweb.ne.jp>
2018-01-11 12:21:25 +0900

    ユーザー登録機能製作中

--- src/com/internousdev/cherry/action/GoUserRegisterAction.java
+++ src/com/internousdev/cherry/action/GoUserRegisterAction.java
@@ -2,10 +2,8 @@ package com.internousdev.cherry.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class UserCreateAction extends ActionSupport{
-
-	public String execute(){
+public class GoUserRegisterAction extends ActionSupport {
+	public String execute() {
 		return SUCCESS;
 	}
-
 }
--- src/com/internousdev/cherry/dao/LoginDAO.java
+++ src/com/internousdev/cherry/dao/LoginDAO.java
@@ -3,39 +3,131 @@ package com.internousdev.cherry.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import com.internousdev.cherry.dto.LoginDTO;
 import com.internousdev.cherry.util.DBConnector;
 
+
 public class LoginDAO {
-	private DBConnector dbConnector = new DBConnector();
-	private Connection connection = dbConnector.getConnection();
-	private LoginDTO loginDTO = new LoginDTO();
 
-	public LoginDTO getLoginUserInfo (String user_id,String password){
-		String sql = "SELECT * FROM login_user_transaction where login_id = ? AND login_pass =?";
-		try{
-			PreparedStatement preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setString(1, user_id);
-			preparedStatement.setString(2, password);
 
-			ResultSet resultSet = preparedStatement.executeQuery();
+	public UserInfoDTO select(String user_id,String password) /*throws SQLException*/ {
+		UserInfoDTO userInfoDTO = new UserInfoDTO();
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
 
-			if(resultSet.next()){
-				loginDTO.setUser_id(resultSet.getString("login_id"));
-				loginDTO.setPassword(resultSet.getString("login_pass"));
+		String sql = "select * from user_info where user_id=? and password=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, user_id);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
 
-				if(!(resultSet.getString("login_id").equals(null))){
-					loginDTO.setLoginFlg(true);
-				}
+			if(rs.next()) {
+				userInfoDTO.setUserId(rs.getString("user_id"));
+				userInfoDTO.setPassword(rs.getString("password"));
+
 			}
-		}catch(Exception e){
+
+		} catch (SQLException e){
 			e.printStackTrace();
-		}
-		return loginDTO;
+
+		} /*finally {
+			con.close();
+		}*/
+
+		return userInfoDTO;
 	}
-	public LoginDTO getLoginDTO(){
-		return loginDTO;
+
+
+	public boolean login(UserInfoDTO userInfoDTO) throws SQLException {
+		boolean result = false;
+		int updateCount = 0;
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "UPDATE user_info SET logined=1 WHERE user_id=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, userInfoDTO.getUser_id());
+			updateCount = ps.executeUpdate();
+			if(updateCount > 0) {
+				System.out.println("ログイン済み");
+				result = true;
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			con.close();
+
+		}
+
+		return result;
+
+	}
+
+	/**
+	 * ログアウト
+	 * @param userInfoDTO
+	 * @return
+	 * @throws SQLException
+	 */
+	public boolean logout(UsernfoDTO userInfoDTO) throws SQLException {
+		boolean result = false;
+		int updateCount = 0;
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "UPDATE user_info SET logined=0 WHERE user_id=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, userInfoDTO.getUser_id());
+			updateCount = ps.executeUpdate();
+			if(updateCount > 0) {
+				System.out.println("未ログイン");
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			con.close();
+		}
+		return result;
+	}
+
+	/**
+	 * ユーザーIDが存在してるか確認
+	 * @param userId
+	 * @return
+	 * @throws SQLException
+	 */
+	public boolean existsUserId(String user_id) throws SQLException {
+		boolean result = false;
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "SELECT * FROM user_info WHERE user_id = ?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, user_id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				result = true;
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			con.close();
+
+		}
+
+		return result;
 	}
 
 }
