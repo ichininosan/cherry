b74d91bcb498600be34ac443d3c773f36edd482d
yu-sakurai <parker.skri@gmail.com>
2018-01-19 13:10:29 +0900

    TopActionでloginFlgとtempUserIdをいじりました。

--- src/com/internousdev/cherry/action/TopAction.java
+++ src/com/internousdev/cherry/action/TopAction.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.internousdev.cherry.dao.Product_InfoDAO;
@@ -19,13 +20,21 @@ public class TopAction extends ActionSupport implements SessionAware {
 
 	public String execute(){
 		Product_InfoDAO dao = new Product_InfoDAO();
+		RandomStringUtils rndStr=new RandomStringUtils();
 		try {
 			proList = dao.pro_list();
 			session.put("proList",proList);
-			//session.put("loginFlg", false);
-			String userId="b";
-			session.put("tempUserId", userId);
-			System.out.println(session.get("tempUserId").toString());
+			if(!(session.containsKey("loginFlg"))){
+				session.put("loginFlg", false);
+				String userId=RandomStringUtils.randomAlphabetic(10);
+				System.out.println(userId);
+				session.put("tempUserId", userId);
+				System.out.println(session.get("tempUserId").toString());
+			}
+
+
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
