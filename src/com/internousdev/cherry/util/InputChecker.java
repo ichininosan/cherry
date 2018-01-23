7a61ba02abeef828c47fc4b4f56342c96c2640c3
Tetsuya-Kudo <10a3019d@gmail.com>
2018-01-23 14:01:52 +0900

    電話番号エラーメッセージ解消

--- src/com/internousdev/cherry/util/InputChecker.java
+++ src/com/internousdev/cherry/util/InputChecker.java
@@ -132,8 +132,8 @@ public class InputChecker {
 
 		if (telNumber.equals("")) {
 			result = "電話番号を入力してください。";
-		} else if (telNumber.length() < 1 || telNumber.length() > 13) {
-			result = "電話番号は1文字以上13文字以下で入力してください。";
+		} else if (telNumber.length() < 11 || telNumber.length() > 13) {
+			result = "電話番号は11文字以上13文字以下で入力してください。";
 		} else if (!telNumber.matches("^[0-9]+$")) {
 			result = "電話番号は半角数字で入力してください。";
 		}
